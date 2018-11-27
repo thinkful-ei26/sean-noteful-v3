e19afbd0445aaa9da724529a8d295d2c8d3731c7
seankrummel <sean.krummel@gmail.com>
2018-11-27 13:43:49 -0600

    seeded the database

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost27017/noteful'
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful'
 };
